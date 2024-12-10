b7ac5a08c12b60afc682315c9374caef0bdaa0f8
TyomoGit <riku3dno.1@gmail.com>
2024-12-10 10:21:28 +0900

    comment

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,27 @@
 import { defineCollection, z } from "astro:content";
 
+/**
+ * マークダウンのFrontmatterに対応する型定義
+ */
 const postsCollection = defineCollection({
     type: "content",
     schema: z.object({
+        /** 記事のタイトル */
         title: z.string(),
+        /** 記事の説明 */
         desc: z.string(),
+        /** 公開日 */
         pubDate: z.date(),
+        /** タグの配列 */
         tags: z.array(z.string()).optional(),
+        /** 記事を表すカバー画像のパス（指定しない場合は、slugから自動生成） */
         cover: z.string().optional(),
+        /** 公開するかどうか */
         pub: z.boolean().optional(),
+        /**
+         * Zenn の記事であるかどうか 
+         * 指定すると、記事の中身は無視され、Zennにリンクされる
+         */
         zenn: z.boolean().optional(),
     })
 });
