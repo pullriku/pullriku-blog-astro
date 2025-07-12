6b5365e3a7d39d37fdd251c4235b5e792761b958
pullriku <riku3dno.1@gmail.com>
2025-07-12 11:55:55 +0900

    post history

--- src/layouts/Prose.tsx
+++ src/layouts/Prose.tsx
@@ -9,7 +9,7 @@ export default function Prose({ children }: ProseProps): JSX.Element {
     return (
         <div
             className="prose dark:prose-invert
-    prose-h1:font-bold prose-h1:text-3xl
+    prose-h1:font-bold prose-h1:text-4xl prose-h1:my-0
   prose-a:text-blue-600 prose-a:no-underline prose-a:hover:underline dark:prose-a:text-blue-400 
     prose:break-words !max-w-none
     prose-img:rounded-xl prose-img:mx-auto prose-img:object-contain prose-img:max-h-[38rem] prose-img:my-0 prose-img:w-auto prose-img:h-auto prose-img:shadow-lg
